26bf54388781570d06e2e17156999ebb3a12b43e
Anatolii Proniuk <anatolii.proniuk@gmail.com>
2020-03-27 17:28:14 +0200

    init commit

---
+++ DAL/Interfaces/IApiRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.DAL.Interfaces
+{
+    interface IApiRequest
+    {
+        void GetCurrentWeather();
+        void GetFiveDayThreeHourWeather();
+        void GetSixteenDaysDailyWeather();
+    }
+}
